461930aacb28595e04839b8ace021092fd8db961
Jacob <j4dwyer@student.bridgew.edu>
2020-02-14 11:05:32 -0500

    Fixed addEntry handler
    still wont display the list in the ListView

--- AddressBook.cpp
+++ AddressBook.cpp
@@ -27,7 +27,7 @@ void AddressBook::SearchAddressBook(std::string name)
 	}
 }
 
-void AddressBook::RemoveAddressBook(std::string name)
+void AddressBook::RemoveEntry(std::string name)
 {
 	for (std::list<Entry*>::iterator currentEntry = this->entries.begin(); currentEntry != this->entries.end(); ++currentEntry) {
 		if ((*currentEntry)->GetName().compare(name) == 0) {
@@ -42,6 +42,5 @@ void AddressBook::PrintAddressBook()
 		std::cout << (*currentEntry)->ToString() << std::endl << "\n";
 		std::string str;
 		str = (*currentEntry)->ToString();
-		//return str;
 	}
 }
